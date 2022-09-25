bd8a7818a50bb7a5703bc35c3033a4bc6605376a
Lucas Araujo <araujolucas994@gmail.com>
2022-09-25 13:23:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+flappy = document.getElementById("flappy");
+score = document.getElementsByClassName("score");
+pipes = document.getElementsByClassName("pipes");
+
+flappy.addEventListener("keydown")
+
+
+flappy.offsetHeight()
+
+
+
+
